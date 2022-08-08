dba0a17584f28f93499d41d9faff7e7088618728
eggy <eggyrules@gmail.com>
2022-08-07 21:25:41 -0400

    feat: add outline 2

---
+++ components/index/about.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="prose dark:prose-invert">
+    <h1>About</h1>
+    <p>incl. projects</p>
+  </div>
+</template>
---
+++ components/index/services.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="prose dark:prose-invert">
+    <h1>Services</h1>
+    <p>Gitea</p>
+    <p>Eifueo</p>
+    <p>Primoprod</p>
+    <p>Calibre</p>
+    <p>Plex</p>
+    <p>Jellyfin</p>
+    <p>Minecraft</p>
+  </div>
+</template>
