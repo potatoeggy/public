2e4e4727d15933a49bc943e849a30766c745bf75
eggy <eggyrules@gmail.com>
2022-08-10 19:01:20 -0400

    fix: give up and make multiple 404s

--- error.vue
+++ error.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
 import Default from "./layouts/default.vue";
+import FourOhFour from "./pages/404.vue";
 </script>
 
 <template>
   <Default>
-    <main class="flex flex-col grow prose dark:prose-invert">
-      <h1 class="mb-0">404 - Not Found</h1>
-      <p>The page you are looking for cannot be found.</p>
-    </main>
+    <FourOhFour />
   </Default>
 </template>
