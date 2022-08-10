6f8f5dad7d485660993645b2d9336f4efdf26347
eggy <eggyrules@gmail.com>
2022-08-10 18:56:06 -0400

    fix: properly 404

--- error.vue
+++ error.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import Default from "./default.vue";
+import Default from "./layouts/default.vue";
 </script>
 
 <template>
   <Default>
-    <main class="flex flex-col grow">
-      <h1>404 - Not Found</h1>
+    <main class="flex flex-col grow prose dark:prose-invert">
+      <h1 class="mb-0">404 - Not Found</h1>
       <p>The page you are looking for cannot be found.</p>
     </main>
   </Default>
