8f65123f329e9b4a9d451193e9903981154a49f1
eggy <eggyrules@gmail.com>
2022-09-07 14:41:24 -0400

    fix: load current page if Nuxt 3 version selected
    Originally, it would attempt to load BASE_URL/Nuxt 3 (2022), which obviously does not exist.

--- data/siteRevisions.ts
+++ data/siteRevisions.ts
@@ -1,11 +1,12 @@
 interface SiteRevision {
   title: string;
-  url?: string;
+  url: string;
 }
 
 export const revisions: SiteRevision[] = [
   {
     title: "Nuxt 3 (2022)",
+    url: "https://eggworld.tk",
   },
   { title: "Eleventy (2021)", url: "https://2021.eggworld.tk" },
   { title: "Vanilla (2019-2020)", url: "https://2020.eggworld.tk" },
