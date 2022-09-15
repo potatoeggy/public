1c6eb2d6fa6744a8e557eb77bda15d6bb55dd143
eggy <eggyrules@gmail.com>
2022-09-15 08:21:52 -0400

    feat: remove nuxt-zero-js and version bump nuxt
    Nuxt rc10 adds a noScript flag, which does the same thing. Remove unnecessary dependencies.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,6 @@ export default defineNuxtConfig({
     "@nuxtjs/color-mode",
     "@nuxtjs/sitemap",
     "nuxt-full-static",
-    "nuxt-zero-js",
   ],
   nitro: {
     prerender: {
@@ -90,5 +89,6 @@ export default defineNuxtConfig({
   },
   experimental: {
     reactivityTransform: true,
+    noScripts: true,
   },
 });
