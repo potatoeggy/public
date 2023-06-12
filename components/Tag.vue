a5f8b4ca3017bc7a2db1094da4082b2c2de66c1a
eggy <danielchen04@hotmail.ca>
2023-06-11 21:20:41 -0400

    feat: allow tags with spaces to be linkable
    something like 'unstagnation 2023'

--- components/Tag.vue
+++ components/Tag.vue
@@ -5,7 +5,8 @@ const props = defineProps<{
   highlight?: boolean;
 }>();
 
-const isLinkableTag = !props.name.includes(" ");
+// const isLinkableTag = !props.name.includes(" ");
+const isLinkableTag = true;
 const tagClass = [
   "inline-block text-xs rounded-lg py-1 px-2 mt-1 mr-1 transition border border-pink-200 dark:border-pink-900 border-2 font-medium",
   { "bg-pink-200 dark:bg-pink-900": props.highlight },
