2ad9e41c8bbc79f56781e0555a8c2beca919525e
eggy <d7chen@uwaterloo.ca>
2024-02-29 18:38:15 -0500

    fix: show correct featured works

--- components/PostPreviewCard.vue
+++ components/PostPreviewCard.vue
@@ -22,7 +22,10 @@ const descText =
     class="break-words max-w-full rounded-lg p-4 shadow-md border border-2 border-gray-300 dark:border-gray-600"
   >
     <h3 class="m-0 flex items-center gap-1.5">
-      <a :href="`/tags/${props.type}/featured`" v-if="highlighttags?.includes('featured')">
+      <a
+        :href="`/tags/${props.type}/featured`"
+        v-if="post.tags.includes('featured')"
+      >
         <IconStar class="fill-yellow-500 outline-none" />
       </a>
       <a
@@ -35,12 +38,12 @@ const descText =
     <p class="my-1 text-sm"><Date :doc="post" /> · {{ descText }}</p>
     <div class="flex flex-wrap">
       <template v-for="(tag, index) in post.tags" :key="index">
-      <Tag
-        :dest="`/tags/${type}/${tag}`"
-        :name="tag"
-        :highlight="highlighttags?.includes(tag)"
-        v-if="!SpecialTags.includes(tag)"
-      />
+        <Tag
+          :dest="`/tags/${type}/${tag}`"
+          :name="tag"
+          :highlight="highlighttags?.includes(tag)"
+          v-if="!SpecialTags.includes(tag)"
+        />
       </template>
     </div>
     <ContentRenderer :value="post" :excerpt="true" tag="section">
