1108f258c125afed4a292300b77323e493af8cc6
eggy <d7chen@uwaterloo.ca>
2024-05-11 18:05:57 -0400

    fix: move tag anchor inside div

--- components/Tag.vue
+++ components/Tag.vue
@@ -8,20 +8,18 @@ const props = defineProps<{
 // const isLinkableTag = !props.name.includes(" ");
 const isLinkableTag = true;
 const tagClass = [
-  "inline-block text-xs rounded-lg py-1 px-2 mt-1 mr-1 transition border border-pink-200 dark:border-pink-900 border-2 font-medium",
+  "inline-block text-xs rounded-lg py-1 px-2 mt-1 mr-1 transition border border-pink-200 dark:border-pink-900 border-2 font-medium no-underline",
   { "bg-pink-200 dark:bg-pink-900": props.highlight },
   { "shadow-md": isLinkableTag },
 ];
 </script>
 
 <template>
-  <a :href="dest" v-if="isLinkableTag">
-    <div :class="tagClass">
+  <div :class="tagClass">
+    <a :href="dest" v-if="isLinkableTag">
       {{ name }}
-    </div>
-  </a>
-  <div v-else>
-    <div :class="tagClass">
+    </a>
+    <div v-else>
       {{ name }}
     </div>
   </div>
