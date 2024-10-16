247e0dcdc7b318af48a79032829c08c0c1a49135
Daniel Chen <danielchen04@hotmail.ca>
2024-10-16 13:06:19 -0400

    chore: upgrade deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
         { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
         {
           rel: "stylesheet",
-          href: "https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css",
+          href: "https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/katex.min.css",
         },
       ],
       script: [
