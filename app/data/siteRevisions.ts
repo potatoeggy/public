4c1c8ae3af04c499f43b68ae943f0290ce8370e3
eggy <d7chen@uwaterloo.ca>
2025-10-19 15:33:02 +0800

    chore: upgrade to nuxt 4

---
+++ app/data/siteRevisions.ts
@@ -0,0 +1,13 @@
+interface SiteRevision {
+  title: string;
+  url: string;
+}
+
+export const revisions: SiteRevision[] = [
+  {
+    title: "Nuxt 3 (2022)",
+    url: "https://eggipelago.com",
+  },
+  { title: "Eleventy (2021)", url: "https://2021.eggipelago.com" },
+  { title: "Vanilla (2019-2020)", url: "https://2020.eggipelago.com" },
+];
